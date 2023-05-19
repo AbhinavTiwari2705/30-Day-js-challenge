385e64022c294fa185835d557064b1844ba3d1f7
Abhinav Tiwari <97847842+AbhinavTiwari2705@users.noreply.github.com>
2023-05-19 22:06:37 +0530

    Create day14.js

---
+++ day14.js
@@ -0,0 +1,13 @@
+var debounce = function(fn, t) {
+  let interval;
+  return function(...args) {
+    const lastCall = Date.now()
+    clearInterval(interval);
+    interval = setInterval(() => {
+      if (Date.now() - lastCall >= t) {
+        fn(...args);
+        clearInterval(interval);
+      }
+    }, 1);
+  }
+};
